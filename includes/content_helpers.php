0ff428d904d43740e8bdbb8632bac27bfd17584c
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2026-03-28 20:33:43 +0500

    Update content_helpers.php

--- includes/content_helpers.php
+++ includes/content_helpers.php
@@ -16,3 +16,8 @@ function get_flash_notice(PDO $connection): ?array
 {
     return db_select_one($connection, "SELECT * FROM `flash_notice` WHERE `id` = 1");
 }
+
+function get_carousel_images(PDO $connection): array
+{
+    return db_select_all($connection, "SELECT * FROM `carousel_images` WHERE `is_enabled` = 1 ORDER BY `sort_order` ASC, `id` ASC");
+}
